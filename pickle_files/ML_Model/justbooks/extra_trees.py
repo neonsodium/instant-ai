9340514d8eb677a05f2920b8a5728d81e6ea846d
neonsodium <neonsodium747@gmail.com>
2024-11-02 10:40:32 +0530

    commit-00002: removed pickle_files/ML_Model/

--- pickle_files/ML_Model/justbooks/extra_trees.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.ensemble import ExtraTreesRegressor
-
-def extra_trees(X, Y):
-    model = ExtraTreesRegressor()
-    model.fit(X, Y)
-    feature_importances = model.feature_importances_
-    featureScores = pd.DataFrame({'Feature': X.columns, 'Importance': feature_importances})
-    return featureScores.sort_values(by='Importance', ascending=False)
