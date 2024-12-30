5e1fb10f1853849c49ad388431c0a62a96378c6d
neonsodium <20je1069@iitism.ac.in>
2024-12-31 00:35:37 +0530

    commit-00060: Need to redo logging for the entire application.

--- celery_worker.py
+++ celery_worker.py
@@ -4,9 +4,9 @@ from os import getenv
 from app import celery
 from app.logger_config import configure_logging
 
-LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
-LOG_FILE = getenv("LOG_FILE", "celery_app.log")
-configure_logging(LOG_LEVEL, LOG_FILE)
+# LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
+# LOG_FILE = getenv("LOG_FILE", "celery_app.log")
+# configure_logging(LOG_LEVEL, LOG_FILE)
 
 if __name__ == "__main__":
     logging.info("Starting Celery worker...")
--- run.py
+++ run.py
@@ -3,11 +3,11 @@ from os import getenv
 from app import app
 from app.logger_config import configure_logging
 
-LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
-LOG_FILE = getenv("LOG_FILE", "flask_app.log")
-configure_logging(LOG_LEVEL, LOG_FILE)
+# LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
+# LOG_FILE = getenv("LOG_FILE", "flask_app.log")
+# configure_logging(LOG_LEVEL, LOG_FILE)
 
 
 if __name__ == "__main__":
-    app.logger.info("Starting Flask application.")
+    # app.logger.info("Starting Flask application.")
     app.run(host=app.config.get("HOST"), port=app.config.get("PORT"), debug=app.config.get("DEBUG"))
